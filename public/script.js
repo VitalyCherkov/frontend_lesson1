1c787c51df8e5f6e5ce0a75019b5b235b14fef62
Vitaly Cherkov <vv-ch@bk.ru>
2018-02-09 20:31:12 +0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,10 @@
+console.log('source kek 2');
+
+let text = document.getElementById('text');
+console.log(text);
+
+const button = document.getElementById('button');
+button.onclick = () => {
+    console.log('clicked');
+    text.innerText = 'Нажата кнопочка';
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const debug = require('debug');
+const logger = debug('myLogger');
+
+const server = http.createServer((req, resp) => {
+
+    logger(req.url);
+    logger(process.env.DEBUG);
+
+
+    fs.readFile(path.join(__dirname, 'public', req.url), (err, data) => {
+        if(err) {
+            resp.writeHead('404');
+            resp.end();
+            return;
+        }
+        resp.writeHead(200, 'OK');
+        resp.write(data);
+        resp.end();
+    });
+});
+
+server.listen(3000);
+
+
